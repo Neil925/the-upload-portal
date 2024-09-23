2b5d0c953aa04f2ffc9d1f46f04d7bec75da3521
Neil E. <elkadineil@gmail.com>
2024-09-23 18:27:39 -0400

    Video and image section

--- src/modules/DemoUpload.jsx
+++ src/modules/DemoUpload.jsx
@@ -94,7 +94,7 @@ export default function DemoUpload() {
   }
 
   return (
-    <div className='border-2 border-white row-span-2 h-[80vh] lg:h-full'>
+    <div className='row-span-2 h-[80vh] lg:h-full'>
       <div className='h-3/4 '>
         <h2 className='font-bold text-3xl h-[10%]'>Demo Zone</h2>
         <div className='w-full h-[90%] bg-neutral-800 bg-gradient-to-r from-bgGradientR to-bgGradientL place-content-center flex items-center'>
@@ -106,7 +106,7 @@ export default function DemoUpload() {
             />
             <label htmlFor="demo-input" onDragOver={dragOver} onDrop={fileDrop}>
               <div className='outline-8 outline-secondary outline-dashed w-[99%] h-[99%] z-[0] grid grid-cols-1 md:grid-cols-2'>
-                <FaFile className='text-white text-6xl lg:text-8xl self-center mx-auto' />
+                <FaFile className='text-white text-6xl md:text-8xl self-center mx-auto' />
                 <div className='self-center h-full w-full flex items-center place-content-center lg:place-content-start'>
                   <div className='text-xl font-mono'>
                     <p ref={nameSpan} className='font-bold text-3xl'>No File Submitted</p>
@@ -114,11 +114,11 @@ export default function DemoUpload() {
                     <p><b>File type: </b><span ref={fileTypeSpan}></span></p>
                   </div>
                 </div>
-                <div className='flex justify-center lg:col-span-2'>
+                <div className='flex justify-center md:col-span-2'>
                   <div className='w-1/2 flex justify-center items-center'>
-                    <div className='w-32 lg:w-48 h-12 place-content-center'>
+                    <div className='w-32 md:w-48 h-12 place-content-center'>
                       <select
-                        className='z-50 absolute w-32 lg:w-48 h-12 text-right p-2 appearance-none bg-transparent text-transparent'
+                        className='z-50 absolute w-32 md:w-48 h-12 text-right cursor-pointer p-2 appearance-none bg-transparent text-transparent'
                         name="expiration" id="demo-expiration" onChange={handleExpiration} defaultValue="30 minutes">
                         <option value="30 minutes">30 minutes</option>
                         <option value="1 hour">1 hour</option>
@@ -129,7 +129,7 @@ export default function DemoUpload() {
                       <div className='flex h-full'>
                         <div
                           className='bg-primary rounded-l-xl p-2 border-r-4 border-black h-full w-1/4 text-8xl font-bold items-center flex'
-                        ><RiArrowDropDownLine /></div>
+                        ><RiArrowDropDownLine className='scale-[3] md:scale-150' /></div>
                         <div htmlFor="experation" ref={expirationEl}
                           className='text-center text-nowrap overflow-clip text-xl md:text-2xl w-3/4 bg-primary rounded-r-xl p-2 h-full font-bold'>
                           30 minutes
@@ -137,11 +137,11 @@ export default function DemoUpload() {
                       </div>
                     </div>
                   </div>
-                  <div className='w-1/2 place-content-center'>
-                    <div className='text-right bg-primary rounded-lg p-3 lg:p-5 h-fit text-xl flex items-center w-fit cursor-pointer'
+                  <div className='w-1/2 flex justify-center items-center'>
+                    <div className='text-right bg-primary rounded-lg p-3 md:p-5 h-fit text-xl flex items-center w-fit cursor-pointer'
                     >
-                      <FaFileArrowUp className='text-4xl md:text-5xl lg:text-6xl' />
-                      <b className='text-2xl md:text-3xl lg:text-5xl ml-5 w-full text-center select-none'>Upload</b>
+                      <FaFileArrowUp className='text-4xl md:text-6xl' />
+                      <b className='text-2xl md:text-5xl ml-5 w-full text-center select-none'>Upload</b>
                     </div>
                   </div>
                 </div>
@@ -159,7 +159,7 @@ export default function DemoUpload() {
           <input className='w-4/5'
             type="range" min={1} max={5000000000} defaultValue={230000000}
             onChange={handleSliderChange} ref={sizeInputElement} name='demo-slider' />
-          <label htmlFor='demo-slider' className='min-w-fit font-bold font-mono text-center text-2xl' ref={slideTextEl}>230 mb</label>
+          <label htmlFor='demo-slider' className='font-bold font-mono text-center text-lg md:text-2xl w-1/5' ref={slideTextEl}>230 mb</label>
         </div>
       </div>
     </div>
--- src/routes/signup/Signup.jsx
+++ src/routes/signup/Signup.jsx
@@ -2,29 +2,31 @@ import React, { useState } from 'react'
 import PopupForm from '../../modules/popupform/Popupform';
 import Logo from "../../resources/Logo.png";
 import DemoUpload from '../../modules/DemoUpload';
+import placeHolder from '../../resources/placeholder.png';
+import video from '../../resources/SaveClip.App_994B7C08A3A6CE8A66AE9E0946A7DDB9_video_dashinit.mov';
 
 export default function Signup() {
   const [trigger, setTrigger] = useState(0);
 
   return (
-    <div className='py-5 mx-auto px-4 md:px-10 flex-grow text-white'>
+    <div className='flex-grow text-white'>
       <PopupForm trigger={trigger} />
-      <div className='min-h-screen'>
-        <header className=''>
-          <div className='bg-neutral-950 fixed h-full w-full left-0 top-0 z-[-1] bg-[radial-gradient(ellipse_80%_80%_at_50%_50%,#272727,#3E3E3E)]' />
-          <nav className='flex w-full  justify-between'>
-            <img className='w-52' alt="Logo" src={Logo} />
-            <div className='place-content-center'>
-              <button
-                className='p-2 h-fit rounded-md bg-primary'
-                onClick={() => setTrigger(x => x + 1)}
-              ><b className='text-2xl'>Sign Up!</b></button>
-            </div>
-          </nav>
-        </header>
-        <main className='grid grid-cols-1 lg:grid-cols-2'>
-          <div className='border-2 border-white '>
-            <h1 className='text-5xl lg:text-8xl font-bold pt-10'>
+      <header className='pt-5 mx-auto px-4 md:px-10'>
+        <div className='bg-neutral-950 fixed h-full w-full left-0 top-0 z-[-1] bg-[radial-gradient(ellipse_80%_80%_at_50%_50%,#272727,#3E3E3E)]' />
+        <nav className='flex w-full justify-between'>
+          <img className='w-32 md:w-52 object-contain' alt="Logo" src={Logo} />
+          <div className='place-content-center'>
+            <button
+              className='p-2 h-fit rounded-md bg-primary'
+              onClick={() => setTrigger(x => x + 1)}
+            ><b className='text-lg md:text-2xl'>Sign Up!</b></button>
+          </div>
+        </nav>
+      </header>
+      <main className=''>
+        <div className='mx-auto px-4 md:px-10 min-h-[90vh] grid grid-cols-1 lg:grid-cols-2'>
+          <div className='pb-5'>
+            <h1 className='text-5xl md:text-6xl lg:text-8xl font-bold pt-10'>
               <span className='bg-gradient-to-r from-purple-500 via-primary to-purple-800 bg-clip-text text-transparent'>Upload</span> And<br />
               <span className='bg-gradient-to-r from-purple-500 via-primary to-purple-800 bg-clip-text text-transparent'>Share</span> With <br />
               <span className='text-secondary'>Ease</span>!
@@ -32,19 +34,37 @@ export default function Signup() {
             <div className='text-xl lg:text-3xl font-bold mt-5'>Make files simple.</div>
           </div>
           <DemoUpload />
-          <div className='place-content-center align-bottom flex gap-3 border-2 border-white min-h-fit'>
+          <div className='place-content-center font-bold align-bottom flex gap-3 min-h-fit'>
             <div
-              className='bg-secondary font-bold text-lg lg:text-3xl text-center p-2 rounded-xl self-center aspect-square place-items-center flex justify-center items-center'
+              className='bg-secondary text-lg lg:text-3xl text-center p-2 rounded-xl self-center aspect-square place-items-center w-24 lg:w-fit flex justify-center items-center'
             ><p>Share<br />with<br />anyone</p></div>
             <div
-              className='bg-primary font-bold text-lg text-center p-2 rounded-xl self-center mt-24 aspect-square flex justify-center items-center'
+              className='bg-primary text-lg text-center p-2 rounded-xl self-center mt-24 aspect-square flex justify-center items-center'
             ><p>Fast<br />download</p></div>
             <div
-              className='bg-secondary font-bold text-2xl text-center p-2 rounded-xl self-center mb-24 aspect-square place-items-center flex justify-center items-center'
+              className='bg-secondary text-2xl text-center p-2 rounded-xl self-center mb-24 aspect-square place-items-center flex justify-center items-center'
             ><p>Fully<br />encrypted<br />files</p></div>
           </div>
-        </main>
-      </div>
+        </div>
+        <div className='bg-secondary w-full lg:h-[70vh] justify-center p-4'>
+          <div className='flex flex-col space-y-10 md:flex-row h-full justify-evenly md:place-content-center place-items-center'>
+            <div className='w-full md:w-1/2 space-y-3 pr-2'>
+              <div className='flex flex-wrap gap-3 justify-center'>
+                <img alt='img' src={placeHolder} className='w-40 md:w-1/3 self-stretch' />
+                <img alt='img' src={placeHolder} className='w-40 md:w-1/3 self-stretch' />
+                <img alt='img' src={placeHolder} className='w-40 md:w-1/3 self-stretch' />
+                <img alt='img' src={placeHolder} className='w-40 md:w-1/3 self-stretch' />
+                <img alt='img' src={placeHolder} className='w-40 md:w-1/3 self-stretch' />
+              </div>
+            </div>
+            <div className='w-full md:w-1/2 flex justify-center items-center pl-2'>
+              <video alt='demo-video' className='border-2 border-white w-full' controls >
+                <source src={video} />
+              </video >
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   )
 }
